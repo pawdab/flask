e8e4cf8d5fae80ee66f23cf6c434902487c7ed6c
Paweł Dąbrowski <paweldabrowski@zoho.com>
2020-05-24 17:28:17 +0200

    flask

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+from flask import request, redirect
+
+from flask import render_template
+
+@app.route('/me', methods=['GET'])
+def meme():
+       return render_template("me.html")
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contactz():
+   if request.method == 'GET':
+       print("We received GET")
+       return render_template("contact.html")
+   elif request.method == 'POST':
+       print("We received POST")
+       x = str(request.form['element'])
+       print("z formularza:" + x)
+       return redirect("/contact")
+
+
