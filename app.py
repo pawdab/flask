f2fabd051077313d2fd132422802b4522ab1e668
Paweł Dąbrowski <paweldabrowski@zoho.com>
2020-05-25 20:21:55 +0200

    Deleted all print in app.py

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ def meme():
 @app.route('/contact', methods=['GET', 'POST'])
 def contactz():
    if request.method == 'GET':
-       print("We received GET")
+       #print("We received GET")
        return render_template("contact.html")
    elif request.method == 'POST':
-       print("We received POST")
+       #print("We received POST")
        x = str(request.form['element'])
-       print("z formularza:" + x)
+       #print("z formularza:" + x)
        return redirect("/contact")
 
 
